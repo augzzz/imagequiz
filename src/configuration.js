c6d54b1cfbbed56e35e26134f871f80c342943b3
augzzz <amoore124@email.arizona.edu>
2022-05-04 23:44:18 -0700

    updated front end / back end URL's

--- src/configuration.js
+++ src/configuration.js
@@ -1,6 +1,6 @@
 let configuration = {
     //backendAddress: 'http://localhost:4002'
-    backendAddress: 'https://augzzz-imagequiz-api.herokuapp.com'
+    backendAddress: 'https://augzzz-imagequiz-api.herokuapp.com/'
 };
 
 export default configuration;
